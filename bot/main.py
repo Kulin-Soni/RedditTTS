1eda14cd267f56c186d2a30bfbf60c6f17f51374
Kulin-Soni <grozersniper38237@gmail.com>
2026-05-18 01:32:07 +0530

    better env support

--- bot/main.py
+++ bot/main.py
@@ -1,12 +1,13 @@
 import asyncpraw as pw
 from load_dotenv import load_dotenv
-from os import getenv
+from os import environ as env
 
 load_dotenv(".env")
 client = pw.Reddit(
-    client_id=getenv("CLIENT_ID"),
-    client_secret=getenv("CLIENT_SECRET"),
-    password=getenv("PASSWORD"),
-    user_agent=getenv("USERAGENT"),
-    username=getenv("USERNAME"),
+    client_id=env.get("CLIENT_ID"),
+    client_secret=env.get("CLIENT_SECRET"),
+    password=env.get("PASSWORD"),
+    user_agent=env.get("USERAGENT"),
+    username=env.get("USERNAME"),
 )
+
